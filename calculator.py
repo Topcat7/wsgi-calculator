3e8e55f13fe2bd4cbedae40c7394bd2bb0398dd9
Tian Yen <tianchuanyen@hotmail.com>
2018-05-02 16:08:16 -0700

    wsgi-calculator with my code

--- calculator.py
+++ calculator.py
@@ -40,18 +40,64 @@ To submit your homework:
 
 
 """
+def simplified(num_list, operator):
+    sum = int(num_list[0])
+    for i in range(1, len(num_list)):
+        if operator == 'add':
+            sum += int(num_list[i])
+        if operator == 'subtract':
+            sum -= int(num_list[i])
+        if operator == 'multiply':
+            sum *= int(num_list[i])
+        if operator == 'divide':
+            if num_list[i] == 0:
+                raise ZeroDivisionError
+            else:
+                sum /= int(num_list[i])
 
+    return str(int(sum))
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
 
     # TODO: Fill sum with the correct value, based on the
     # args provided.
-    sum = "0"
-
-    return sum
+    return simplified(args, 'add')
 
 # TODO: Add functions for handling more arithmetic operations.
+
+def subtract(*args):
+    """subtract subsequent arguments from the first, return a string of the sum"""
+    return simplified(args, 'subtract')
+
+def multiply(*args):
+    """multiply the arguments together and return the total as a string"""
+    return simplified(args, 'multiply')
+
+def divide(*args):
+    """divide the first argument by the following arguments, raise a ZeroDivisionError if dividing by zero"""
+    return simplified(args, 'divide')
+
+def main_page(*args):
+    return """Welcome to the WSGI calculator,
+
+            please choose from several functions to use,
+
+            You can add, subtract, multiply, or divide!
+
+            <p>Here are some examples:</p>
+
+            <ul>"/add/2/3" Expect: 6</ul>
+
+            <ul>"subtract/5/7" Expect: -2</ul>
+
+            <ul>"multiply/2/2" Expect: 4</ul>
+
+            <ul>"divide/24/6" Expect: 4</ul>
+
+            <p>This calculator works only with integer numbers!!</p>
+
+            """
 
 def resolve_path(path):
     """
@@ -63,8 +109,14 @@ def resolve_path(path):
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
+    args = path.strip("/").split('/')
+    print(args)
+    func_name = args.pop(0)
+    func = {"add": add,
+            "subtract": subtract,
+            "multiply": multiply,
+            "divide": divide,
+            "": main_page}.get(func_name)
 
     return func, args
 
@@ -76,9 +128,31 @@ def application(environ, start_response):
     #
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
-    pass
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = '404 Not Found'
+        body = '<h1>Not Found</h1>'
+    except ZeroDivisionError:
+        status = "400 Bad Request"
+        body = "<h1>Cannot Divide By Zero</h1>"
+    #except Exception:
+        #status = "500 Internal ServerError"
+        #body = "<h1>Internal Server Error</h1>"
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
     # TODO: Insert the same boilerplate wsgiref simple
     # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
